8a4f54725445ddcb6e57d826aec184185d5f9d7e
Ross McDonald <Ross.McDonald@passivsystems.com>
2014-04-25 16:24:14 +0100

    compose functions for common effects

--- src/me/rossputin/pew.clj
+++ src/me/rossputin/pew.clj
@@ -2,25 +2,11 @@
 	(:require [me.raynes.laser :as l]))
 
 ;; =============================================================================
-;; Twitter Bootstrap classes
+;; Twitter Bootstrap class shortcuts
 ;; =============================================================================
 
 (defonce glyph-info "glyphicon glyphicon-info-sign")
 (defonce pnl-info "panel panel-info")
-
-
-;; =============================================================================
-;; Simple elements
-;; =============================================================================
-
-(defn a [h c] (l/node :a :attrs {:href h} :content c))
-(defn ul-unstyled [d] (l/node :ul :attrs {:class "list-unstyled"} :content d))
-(defn li [d] (l/node :li :content d))
-(defn tr [d] (l/node :tr :content d))
-(defn td [d] (l/node :td :content d))
-(defn div ([] (l/node :div)) ([d] (l/node :div :content d)))
-(defn span ([] (l/node :span)) ([d] (l/node :span :content d)))
-(defn small [d] (l/node :small :content d))
 
 
 ;; =============================================================================
@@ -30,6 +16,23 @@
 (defn clazz [classes payload] (assoc-in payload [:attrs :class] classes))
 (defn title [title payload] (assoc-in payload [:attrs :title] title))
 (defn width [width payload] (assoc-in payload [:attrs :width] width))
+
+
+;; =============================================================================
+;; Simple elements
+;; =============================================================================
+
+(defn a [h c] (l/node :a :attrs {:href h} :content c))
+(defn ul [d] (l/node :ul :content d))
+(defn ul-unstyled [d] (->> (ul d) (clazz "list-unstyled")))
+(defn li [d] (l/node :li :content d))
+(defn tr [d] (l/node :tr :content d))
+(defn td [d] (l/node :td :content d))
+(defn div ([] (l/node :div)) ([d] (l/node :div :content d)))
+(defn span ([] (l/node :span)) ([d] (l/node :span :content d)))
+(defn small [d] (l/node :small :content d))
+
+;:attrs {:class "list-unstyled"}
 
 
 ;; =============================================================================
